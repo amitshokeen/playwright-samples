1eb8dfa1b73c05f2af64c6abf0c2384816c3ccc4
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2023-06-27 19:44:25 +1000

    some addition to windows tests

--- tests/windows.test.ts
+++ tests/windows.test.ts
@@ -1,4 +1,6 @@
-import { test, expect, chromium } from "@playwright/test";
+import { test, expect, chromium, Page } from "@playwright/test";
+
+let facebookPage: Page;
 
 test("Interact with multiple tabs", async () => {
     const browser = await chromium.launch({
@@ -35,9 +37,24 @@ test("Interact with button that causes multiple tabs to open", async () => {
     await page.waitForLoadState();
     const pages = multiPage.context().pages();
     console.log('No. of tabs: ' + pages.length);
-
     
     pages.forEach(tab => {
         console.log(tab.url());
     })
+
+    
+
+    // now to interact with the pages, you may use index numbers that denote the individual page.
+    // see the example below:
+    // await pages[1].fill("<some locator>", "Amit");
+
+    for (let index = 0; index < pages.length; index++) {
+        const element = pages[index];
+        const url = pages[index].url()
+        if(url == "https://www.facebook.com/lambdatest/") {
+            facebookPage = pages[index];
+        }
+    }
+    const text = await facebookPage.textContent("h1");
+    console.log(text);
 })
